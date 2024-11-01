a199aff2ef5bc62b28ea8c61160f050583022823
Yong-ga-ri <rlfgks97@gmail.com>
2024-11-01 22:59:32 +0900

    init

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from '@/router/routes'
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+const pinia = createPinia();
+
+app.use(pinia)
+app.use(router)
+
+app.mount('#app')
