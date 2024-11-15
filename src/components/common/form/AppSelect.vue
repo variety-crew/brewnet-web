e68b0e4faf5a0ad2964311a68f03647b3e77cd9a
yanghyeonjin <guswls7081@gmail.com>
2024-11-15 12:00:04 +0900

    feat: AppSelect 컴포넌트 개발 완료

---
+++ src/components/common/form/AppSelect.vue
@@ -0,0 +1,56 @@
+<template>
+  <Select
+    :options="options"
+    :value="modelValue"
+    :default-value="initialValue"
+    option-label="label"
+    option-value="value"
+    v-bind="$attrs"
+    size="small"
+    checkmark
+    placeholder="===선택해주세요==="
+    :highlight-on-select="false"
+    @change="onSelectChange"
+  />
+
+  <!--   
+  v-bind="$attrs": 부모에게서 전달된 추가 속성을 수용
+   -->
+</template>
+
+<script setup>
+const { modelValue, options, initialValue } = defineProps({
+  // 부모로부터 현재 선택된 값을 받아옴
+  modelValue: {
+    type: String,
+    required: true,
+  },
+
+  options: {
+    type: Array,
+    required: true,
+  },
+
+  /**
+   * options: [{
+   *   label: string
+   *   value: string
+   * }]
+   */
+
+  initialValue: {
+    type: String,
+    required: false,
+    default: '',
+  },
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+// 선택이 변경될 때 상위 컴포넌트로 새로운 값 전달
+function onSelectChange(event) {
+  emit('update:modelValue', event.value);
+}
+</script>
+
+<style scoped></style>
--- src/components/headQuarter/EditMemberRoleModalBody.vue
+++ src/components/headQuarter/EditMemberRoleModalBody.vue
@@ -2,15 +2,7 @@
   <AppModalBody action-button-label="저장" @click-action-button="clickSave">
     <template v-if="currentMember">
       <p class="mb-3">[{{ currentMember.name }}] 임직원의 권한을 변경합니다.</p>
-      <Select
-        v-model="selectedRole"
-        :options="roleOptions"
-        option-label="name"
-        placeholder="===선택해주세요==="
-        checkmark
-        :highlight-on-select="false"
-        size="small"
-      />
+      <AppSelect v-model="selectedRole" :options="roleOptions" :initial-value="initialRole" />
     </template>
   </AppModalBody>
 </template>
@@ -18,44 +10,41 @@
 <script setup>
 import { computed, inject, onMounted, ref } from 'vue';
 
+import AppModalBody from '@/components/common/AppModalBody.vue';
+import AppSelect from '@/components/common/form/AppSelect.vue';
 import { HEAD_QUARTER_ROLES } from '@/utils/constant';
 import { formatKoMemberRole } from '@/utils/format';
 
-import AppModalBody from '../common/AppModalBody.vue';
-
-let memberCode = 0;
-
 const dialogRef = inject('dialogRef');
 const currentMember = ref(null);
-const selectedRole = ref(null);
+const selectedRole = ref('');
+const initialRole = computed(() => {
+  return currentMember.value?.role || '';
+});
 const roleOptions = computed(() => {
   const headQuarterRoleOptions = HEAD_QUARTER_ROLES.map(e => ({
-    key: e,
-    name: formatKoMemberRole(e),
+    value: e,
+    label: formatKoMemberRole(e),
   }));
 
   // 현재 권한이 있는 경우에만 "권한 없음" 옵션 표시
   if (currentMember.value?.role) {
-    return headQuarterRoleOptions.concat({ key: 'null', name: '권한 없음' });
+    return headQuarterRoleOptions.concat({ value: 'null', label: '권한 없음' });
   }
 
   return headQuarterRoleOptions;
 });
 
 const clickSave = () => {
-  console.log(memberCode, '권한 저장');
+  console.log(currentMember.value?.code, '권한 저장');
 
   dialogRef.value.close();
 };
 
 onMounted(() => {
+  // 현재 member 권한 가져오기
   const { member } = dialogRef.value.data;
   currentMember.value = member;
-
-  // 현재 권한을 default selected 값으로 설정
-  if (member.role) {
-    selectedRole.value = { key: member.role, name: formatKoMemberRole(member.role) };
-  }
 });
 </script>
 
