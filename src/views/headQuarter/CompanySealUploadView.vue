f559be4eee51013dc58bd49fcb291079fc0551be
yanghyeonjin <guswls7081@gmail.com>
2024-11-18 02:03:06 +0900

    feat: 법인인감 업로드 UI 완료 #21

--- src/views/headQuarter/CompanySealUploadView.vue
+++ src/views/headQuarter/CompanySealUploadView.vue
@@ -1,7 +1,64 @@
 <template>
-  <div>법인인감 등록</div>
+  <div class="company-seal-upload-container">
+    <Button size="small" label="파일 선택" variant="outlined" severity="secondary" @click="clickChoose" />
+    <input ref="inputRef" type="file" accept="image/*" style="display: none" @change="changeFile" />
+
+    <div v-if="previewUrl" class="uploaded-area">
+      <img v-if="previewUrl" :src="previewUrl" alt="업로드 된 이미지" />
+      <Button size="small" label="저장" @click="saveImage" />
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onUnmounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
 
-<style scoped></style>
+const router = useRouter();
+
+const previewUrl = ref(null);
+const inputRef = ref(null);
+
+const clickChoose = () => {
+  inputRef.value?.click();
+};
+
+const changeFile = event => {
+  const { files } = event.target;
+  if (files.length > 0) {
+    const file = files[0];
+    URL.revokeObjectURL(previewUrl); // 기존 파일 지우고
+    previewUrl.value = URL.createObjectURL(file); // 새로운 파일로 교체
+  }
+};
+
+const saveImage = () => {
+  // 법인인감 이미지 저장 API
+  router.replace({ name: 'hq:settings:company-seal' });
+};
+
+onUnmounted(() => {
+  // 남아있는 리소스 정리
+  URL.revokeObjectURL(previewUrl);
+});
+</script>
+
+<style scoped>
+.company-seal-upload-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  .uploaded-area {
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+
+    img {
+      width: 250px;
+      height: 250px;
+    }
+  }
+}
+</style>
