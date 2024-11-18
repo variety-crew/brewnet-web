27eaa9e231b9dc1faa33589325bc00e1b48340b2
yanghyeonjin <guswls7081@gmail.com>
2024-11-18 12:35:30 +0900

    feat: 내 정보 조회 UI 완료 #25

--- src/stores/user.js
+++ src/stores/user.js
@@ -8,24 +8,35 @@ export const useUserStore = defineStore(
     const username = ref('');
     const accessToken = ref('');
     const userType = ref(''); // hq: 본사, fc: 가맹점, d: 배송기사
+    const email = ref('');
+    const phone = ref('');
+    const position = ref('');
+    const signUrl = ref('');
 
     const loginByHeadQuarter = () => {
       loginId.value = 'iamhq';
       username.value = '나는야본사';
       accessToken.value = 'faketoken1';
       userType.value = 'hq';
+      email.value = 'email@company.co.kr';
+      phone.value = '010-1111-1111';
+      position.value = 'MANAGER';
     };
     const loginByFranchise = () => {
       loginId.value = 'iamfc';
       username.value = '나는야가맹점';
       accessToken.value = 'faketoken2';
       userType.value = 'fc';
+      email.value = 'email@company.co.kr';
+      phone.value = '010-1111-1111';
     };
     const loginByDelivery = () => {
       loginId.value = 'iamdelivery';
       username.value = '나는야배송기사';
       accessToken.value = 'faketoken3';
       userType.value = 'd';
+      email.value = 'email@company.co.kr';
+      phone.value = '010-1111-1111';
     };
 
     const logout = () => {
@@ -33,6 +44,10 @@ export const useUserStore = defineStore(
       username.value = '';
       accessToken.value = '';
       userType.value = '';
+      email.value = '';
+      phone.value = '';
+      position.value = '';
+      signUrl.value = '';
     };
 
     return {
@@ -40,6 +55,10 @@ export const useUserStore = defineStore(
       username,
       accessToken,
       userType,
+      email,
+      phone,
+      position,
+      signUrl,
       loginByHeadQuarter,
       loginByFranchise,
       loginByDelivery,
--- src/views/headQuarter/MyInfoView.vue
+++ src/views/headQuarter/MyInfoView.vue
@@ -1,7 +1,38 @@
 <template>
-  <div>마이페이지</div>
+  <div class="my-info-container">
+    <AppLabelText label="아이디" :text="userStore.loginId" />
+    <AppLabelText label="성명" :text="userStore.username" />
+    <AppLabelText label="이메일" :text="userStore.email" />
+    <AppLabelText label="휴대폰번호" :text="userStore.phone" />
+    <AppLabelText label="직급" :text="formatKoEmployeePosition(userStore.position)" />
+    <AppLabelText label="서명" :text="userStore.signUrl || '등록된 서명이 없습니다.'" />
+
+    <div class="button-area">
+      <Button size="small" label="비밀번호 변경" variant="outlined" severity="secondary" />
+      <Button size="small" label="서명 업로드" variant="outlined" severity="secondary" />
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import AppLabelText from '@/components/common/AppLabelText.vue';
+import { useUserStore } from '@/stores/user';
+import { formatKoEmployeePosition } from '@/utils/format';
 
-<style scoped></style>
+const userStore = useUserStore();
+</script>
+
+<style scoped>
+.my-info-container {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  .button-area {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    margin-top: 30px;
+  }
+}
+</style>
