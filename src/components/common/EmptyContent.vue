04accde4e1211bcbd85462fb22c861fe8095d117
yanghyeonjin <guswls7081@gmail.com>
2024-11-19 11:22:13 +0900

    feat: EmptyContent 컴포넌트 추가

---
+++ src/components/common/EmptyContent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="empty-content-container">
+    <i class="pi pi-times-circle"></i>
+    <p>{{ text }}</p>
+    <Button size="small" :label="fallbackLabel" @click="emit('fallback')" />
+  </div>
+</template>
+
+<script setup>
+const { fallbackLabel, text } = defineProps({
+  fallbackLabel: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(['fallback']);
+</script>
+
+<style scoped>
+.empty-content-container {
+  border-radius: 5px;
+  border: 1px solid var(--p-content-border-color);
+  padding: 32px 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  align-items: center;
+
+  i {
+    font-size: 50px;
+    color: var(--p-surface-500);
+  }
+
+  p {
+    color: var(--p-surface-700);
+  }
+}
+</style>
--- src/views/headQuarter/CompanySealView.vue
+++ src/views/headQuarter/CompanySealView.vue
@@ -1,43 +1,27 @@
 <template>
   <div class="company-seal-container">
     <img v-if="sealImageUrl" :src="sealImageUrl" alt="법인인감 이미지" />
-    <div v-else class="empty">
-      <i class="pi pi-times-circle"></i>
-      <p>등록된 법인인감이 없습니다.</p>
-      <Button size="small" label="등록하기" as="router-link" :to="{ name: 'hq:settings:company-seal:upload' }" />
-    </div>
+    <EmptyContent v-else text="등록된 법인인감이 없습니다." fallback-label="등록하기" @fallback="goUpload" />
   </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+import EmptyContent from '@/components/common/EmptyContent.vue';
+
+const router = useRouter();
 
 const sealImageUrl = ref('');
+
+const goUpload = () => {
+  router.push({ name: 'hq:settings:company-seal:upload' });
+};
 
 onMounted(() => {
   // 인감 이미지 가져오기
 });
 </script>
 
-<style scoped>
-.company-seal-container {
-  .empty {
-    border-radius: 5px;
-    border: 1px solid var(--p-content-border-color);
-    padding: 32px 16px;
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
-    align-items: center;
-
-    i {
-      font-size: 50px;
-      color: var(--p-surface-500);
-    }
-
-    p {
-      color: var(--p-surface-700);
-    }
-  }
-}
-</style>
+<style scoped></style>
