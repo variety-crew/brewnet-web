2ab68038e0334d30c67410c284bf1384c64e0b75
yanghyeonjin <guswls7081@gmail.com>
2024-11-19 09:47:58 +0900

    feat: 공지사항 목록 테이블 UI 완료 #31

--- src/views/headQuarter/NoticeListView.vue
+++ src/views/headQuarter/NoticeListView.vue
@@ -1,7 +1,45 @@
 <template>
-  <div>공지사항 목록</div>
+  <div>
+    <AppTable
+      :columns="columns"
+      :paginated-data="paginatedNotices"
+      :total-elements="notices.length"
+      @change-page="onChangePage"
+      @reload="reload"
+    />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed, onMounted, ref } from 'vue';
+
+import AppTable from '@/components/common/AppTable.vue';
+import { mockupNotices } from '@/utils/mockup';
+
+const notices = ref([]);
+const paginatedNotices = computed(() => {
+  return notices.value.slice(0, 15);
+});
+
+const columns = [
+  { field: 'code', header: '공지사항 코드' },
+  { field: 'title', header: '제목' },
+  { field: 'author', header: '작성자' },
+  { field: 'createdAt', header: '작성일시', sortable: true },
+];
+
+const onChangePage = event => {
+  const { page } = event;
+  console.log(page, '페이지로 변경되었다!');
+};
+
+const reload = () => {
+  console.log('reload 테이블');
+};
+
+onMounted(() => {
+  notices.value = [...mockupNotices];
+});
+</script>
 
 <style scoped></style>
