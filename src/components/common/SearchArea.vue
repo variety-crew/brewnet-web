d10345967aed665299206947c1cf125e5fb764cd
yanghyeonjin <guswls7081@gmail.com>
2024-11-21 09:21:16 +0900

    refactor: SearchArea box shadow 적용

--- src/components/common/SearchArea.vue
+++ src/components/common/SearchArea.vue
@@ -12,6 +12,7 @@
   border-radius: 5px;
   border: 1px solid var(--p-surface-300);
   margin-bottom: 32px;
+  box-shadow: var(--p-primary-100) 0px 2px 8px 0px;
 
   /* grid 영역 설정 */
   display: grid;
