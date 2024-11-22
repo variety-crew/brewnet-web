36c8ba640f60806c005eeedbb9a800617f03fd49
yanghyeonjin <guswls7081@gmail.com>
2024-11-22 11:04:56 +0900

    refactor: SearchArea는 form submit으로 검색하게 수정

--- src/components/common/SearchArea.vue
+++ src/components/common/SearchArea.vue
@@ -1,13 +1,17 @@
 <template>
-  <div
-    class="search-area"
-    :class="{ grid }"
-    :style="{
-      gridTemplateColumns: `repeat(${gridRepeatCount}, ${gridRepeatWidth})`,
-    }"
-  >
-    <slot></slot>
-  </div>
+  <form class="search-area-form" @submit.prevent="onSubmit">
+    <div
+      class="search-area"
+      :class="{ grid }"
+      :style="{
+        gridTemplateColumns: `repeat(${gridRepeatCount}, ${gridRepeatWidth})`,
+      }"
+    >
+      <slot></slot>
+    </div>
+
+    <Button type="submit" label="조회" icon="pi pi-search" size="small" fluid />
+  </form>
 </template>
 
 <script setup>
@@ -28,19 +32,28 @@ const { grid, gridRepeatCount, gridRepeatWidth } = defineProps({
     default: '1fr',
   },
 });
+const emit = defineEmits(['search']);
+
+const onSubmit = () => {
+  emit('search');
+};
 </script>
 
 <style scoped>
-.search-area {
+.search-area-form {
   padding: 16px;
   border-radius: 5px;
   border: 1px solid var(--p-surface-300);
   margin-bottom: 32px;
   box-shadow: var(--p-primary-100) 0px 2px 8px 0px;
 
-  &.grid {
-    display: grid;
-    gap: 16px;
+  .search-area {
+    margin-bottom: 20px;
+
+    &.grid {
+      display: grid;
+      gap: 16px;
+    }
   }
 }
 </style>
