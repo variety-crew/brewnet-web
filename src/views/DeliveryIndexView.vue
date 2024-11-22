6e9a987761c35170026875207544683ff5268edb
yanghyeonjin <guswls7081@gmail.com>
2024-11-23 04:09:51 +0900

    feat: 배송기사 페이지 레이아웃 완료

--- src/views/DeliveryIndexView.vue
+++ src/views/DeliveryIndexView.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>배송 기본 레이아웃 페이지</div>
-  <div>
-    <Button label="로그아웃" @click="clickLogout" />
+  <div class="delivery-main-container">
+    <header>
+      <img src="@/assets/images/logo.png" alt="Logo" class="logo" />
+      <Button label="로그아웃" variant="text" size="small" @click="clickLogout" />
+    </header>
+    <main>
+      <RouterView />
+    </main>
+    <footer>
+      <nav>
+        <RouterLink
+          v-for="navItem in navItems"
+          :key="navItem.routerName"
+          :to="{ name: navItem.routerName }"
+          class="tab-link"
+          active-class="active"
+          >{{ navItem.title }}</RouterLink
+        >
+      </nav>
+    </footer>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 import { useUserStore } from '@/stores/user';
@@ -16,6 +34,12 @@ const router = useRouter();
 
 const authApi = new AuthApi();
 
+const navItems = ref([
+  { routerName: 'd:home', title: '주문' },
+  { routerName: 'd:exchange-return', title: '교환/반품' },
+  { routerName: 'd:complete', title: '배송완료' },
+]);
+
 const clickLogout = () => {
   authApi.logout().then(() => {
     userStore.clearUserData();
@@ -24,4 +48,50 @@ const clickLogout = () => {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.delivery-main-container {
+  display: flex;
+  flex-direction: column;
+  width: 600px;
+  height: 100dvh;
+  margin: 0 auto;
+  box-shadow: 0 0 4px 2px rgba(0, 0, 0, 0.2);
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 16px;
+    border-bottom: 2px solid var(--p-surface-100);
+
+    .logo {
+      width: 50px;
+      height: 50px;
+    }
+  }
+
+  main {
+    flex-grow: 1;
+  }
+
+  footer {
+    border-top: 2px solid var(--p-surface-100);
+
+    nav {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+    }
+    .tab-link {
+      text-align: center;
+      color: var(--p-surface-700);
+      padding: 16px 0;
+
+      &.active {
+        background-color: var(--p-primary-600);
+        color: white;
+        font-weight: bold;
+      }
+    }
+  }
+}
+</style>
