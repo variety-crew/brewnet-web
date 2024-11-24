76a6dc557ee05d6bc6ed3787cd7e74335a7a806c
hoddeok07 <jhj2969@naver.com>
2024-11-24 23:09:15 +0900

    feat: 창고별 상품 재고 대시보드 구현

--- src/utils/api/HQStorageApi.js
+++ src/utils/api/HQStorageApi.js
@@ -21,4 +21,26 @@ export default class HQStorageApi extends BaseApiService {
 
     return this.get(`?${searchParams.toString()}`);
   }
+
+  // 창고명 리스트 조회
+  getStoraeNames() {
+    return this.get('/storages');
+  }
+
+  // 창고별 상품 및 재고 목록 조회
+  getStock({ page = 1, pageSize = 15, storageCode = 1, itemName = '' }) {
+    const searchParams = new URLSearchParams();
+    searchParams.append('pageNumber', page);
+    searchParams.append('pageSize', pageSize);
+
+    if (storageCode) {
+      searchParams.append('storageCode', storageCode);
+    }
+
+    if (itemName) {
+      searchParams.append('itemName', itemName);
+    }
+
+    return this.get(`/storage-stock?${searchParams.toString()}`);
+  }
 }
