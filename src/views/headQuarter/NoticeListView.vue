3209c10ce556cf42e2f93dfd2522ccfd482b8ff9
yanghyeonjin <guswls7081@gmail.com>
2024-11-26 17:05:35 +0900

    feat: 공지사항 목록 조회 API 연동 #31

--- src/views/headQuarter/NoticeListView.vue
+++ src/views/headQuarter/NoticeListView.vue
@@ -1,43 +1,69 @@
 <template>
   <div>
-    <SearchArea grid>
-      <AppInputText v-model="titleKeyword" label="제목" />
-      <AppInputText v-model="authorKeyword" label="작성자" />
+    <SearchArea grid @search="onSearch" @form-reset="onReset">
+      <AppInputText v-model="criteria.titleKeyword" label="제목" />
+      <AppInputText v-model="criteria.authorKeyword" label="작성자" />
     </SearchArea>
 
     <AppTable
       :columns="columns"
       :paginated-data="paginatedNotices"
-      :total-elements="notices.length"
+      :total-elements="totalElements"
       :rows-per-page="pageSize"
       @change-page="onChangePage"
       @reload="reload"
+      @change-sort="onChangeSort"
     />
   </div>
 </template>
 
 <script setup>
 import { useToast } from 'primevue';
-import { computed, onMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 import AppTable from '@/components/common/AppTable.vue';
 import AppInputText from '@/components/common/form/AppInputText.vue';
 import SearchArea from '@/components/common/SearchArea.vue';
 import { useAppConfirmModal } from '@/hooks/useAppConfirmModal';
-import { mockupNotices } from '@/utils/mockup';
+import HQNoticeApi from '@/utils/api/HQNoticeApi';
+import { SORTING_OPTION } from '@/utils/constant';
 
 const router = useRouter();
 const { showConfirm } = useAppConfirmModal();
 const toast = useToast();
 
-const notices = ref([]);
-const paginatedNotices = computed(() => {
-  return notices.value.slice(0, 15);
-});
+const page = ref(0);
+const paginatedNotices = ref([]);
 const pageSize = ref(15);
-const titleKeyword = ref('');
-const authorKeyword = ref('');
+const getInitialCriteria = () => ({
+  titleKeyword: '',
+  authorKeyword: '',
+});
+const totalElements = ref(0);
+const criteria = ref(getInitialCriteria());
+const sorting = ref(SORTING_OPTION.DATE_DESC);
+
+const hqNoticeApi = new HQNoticeApi();
+
+const getNoticeList = () => {
+  hqNoticeApi
+    .getNoticeList({
+      page: page.value,
+      pageSize: pageSize.value,
+      sort: sorting.value,
+      title: criteria.value.titleKeyword,
+      writerName: criteria.value.authorKeyword,
+    })
+    .then(data => {
+      paginatedNotices.value = data.content;
+      totalElements.value = data.totalElements;
+    })
+    .catch(e => {
+      paginatedNotices.value = [];
+      totalElements.value = 0;
+    });
+};
 
 const clickDetail = data => {
   // 공지사항 상세보기
@@ -63,9 +89,9 @@ const clickDelete = () => {
 };
 
 const columns = [
-  { field: 'code', header: '공지사항 코드' },
+  { field: 'noticeCode', header: '공지사항 코드' },
   { field: 'title', header: '제목' },
-  { field: 'author', header: '작성자' },
+  { field: 'memberName', header: '작성자' },
   { field: 'createdAt', header: '작성일시', sortable: true },
   {
     field: '',
@@ -90,16 +116,41 @@ const columns = [
 ];
 
 const onChangePage = event => {
-  const { page } = event;
-  console.log(page, '페이지로 변경되었다!');
+  page.value = event.page;
+  getNoticeList();
 };
 
 const reload = () => {
-  console.log('reload 테이블');
+  getNoticeList();
+};
+
+const onSearch = () => {
+  getNoticeList();
+};
+
+const onReset = () => {
+  criteria.value = getInitialCriteria();
+  sorting.value = SORTING_OPTION.DATE_DESC;
+  page.value = 0;
+  getNoticeList();
+};
+
+const onChangeSort = (sortField, sortOrder) => {
+  if (!sortField || !sortOrder) return;
+
+  if (sortField === 'createdAt') {
+    if (sortOrder === 1) {
+      sorting.value = SORTING_OPTION.DATE_ASC;
+    } else if (sortOrder === -1) {
+      sorting.value = SORTING_OPTION.DATE_DESC;
+    }
+  }
+
+  getNoticeList();
 };
 
 onMounted(() => {
-  notices.value = [...mockupNotices];
+  getNoticeList();
 });
 </script>
 
