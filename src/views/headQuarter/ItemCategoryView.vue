a97df10804e666c7a5b50b2193acb9f8c675a1b6
yanghyeonjin <guswls7081@gmail.com>
2024-11-28 11:00:52 +0900

    feat: 품목 카테고리 등록 UI 완료 #89

--- src/views/headQuarter/ItemCategoryView.vue
+++ src/views/headQuarter/ItemCategoryView.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <div class="add-button-wrapper">
+      <Button label="카테고리 추가" icon="pi pi-plus" size="small" @click="clickAddCategory" />
+    </div>
+
     <ItemCategoryList
       title="비식품"
       :categories="nonFoodCategories"
@@ -8,15 +12,33 @@
       @remove-category="onRemove"
     />
     <ItemCategoryList title="식품" :categories="foodCategories" @save-category="onSave" @remove-category="onRemove" />
+
+    <Dialog
+      v-model:visible="showAddCategoryModal"
+      modal
+      :draggable="false"
+      header="품목 카테고리 추가"
+      style="width: 50vw"
+    >
+      <AppSelect v-model="selectedSuperCategory" label="상위카테고리" :options="superCategoryOptions" class="mb-3" />
+      <AppInputText v-model="addingCategoryName" label="카테고리명" />
+
+      <template #footer>
+        <Button label="저장" @click="clickSaveAdd" />
+      </template>
+    </Dialog>
   </div>
 </template>
 
 <script setup>
 import { useToast } from 'primevue';
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 
+import AppInputText from '@/components/common/form/AppInputText.vue';
+import AppSelect from '@/components/common/form/AppSelect.vue';
 import ItemCategoryList from '@/components/headQuarter/ItemCategoryList.vue';
 import { useAppConfirmModal } from '@/hooks/useAppConfirmModal';
+import { makeSelectOption } from '@/utils/helper';
 import { mockupFoodItemCategories, mockupNonFoodItemCategories } from '@/utils/mockup';
 
 const { showConfirm } = useAppConfirmModal();
@@ -24,14 +46,21 @@ const toast = useToast();
 
 const nonFoodCategories = ref([]);
 const foodCategories = ref([]);
+const showAddCategoryModal = ref(false);
+const addingCategoryName = ref('');
+const selectedSuperCategory = ref('');
+const superCategoryList = ref([]);
+const superCategoryOptions = computed(() => {
+  return superCategoryList.value.map(e => makeSelectOption(e, e));
+});
 
 const onSave = targetCategory => {
-  // 카테고리 저장 API
+  // TODO 카테고리 수정 API
   toast.add({ severity: 'success', summary: '처리 성공', detail: '카테고리명이 수정되었습니다.', life: 3000 });
 };
 
 const removeCategory = targetCategory => {
-  // 카테고리 삭제 API
+  // TODO 카테고리 삭제 API
 };
 
 const onRemove = targetCategory => {
@@ -44,10 +73,35 @@ const onRemove = targetCategory => {
   });
 };
 
+const clickAddCategory = () => {
+  showAddCategoryModal.value = true;
+};
+
+const clickSaveAdd = () => {
+  if (!addingCategoryName.value) {
+    toast.add({ severity: 'error', summary: '입력 확인', detail: '카테고리명을 입력해주세요.', life: 3000 });
+    return;
+  }
+
+  // TODO 카테고리 등록 API
+  toast.add({ severity: 'success', summary: '처리 성공', detail: '새 카테고리가 등록되었습니다.', life: 3000 });
+  showAddCategoryModal.value = false;
+
+  // TODO reload
+};
+
 onMounted(() => {
   nonFoodCategories.value = [...mockupNonFoodItemCategories];
   foodCategories.value = [...mockupFoodItemCategories];
+
+  superCategoryList.value = ['NON_FOOD', 'FOOD'];
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.add-button-wrapper {
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 32px;
+}
+</style>
