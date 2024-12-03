adb35b0a621ea8fdc9a13fdd4167dd72f838253b
yanghyeonjin <guswls7081@gmail.com>
2024-12-03 14:46:06 +0900

    feat: [가맹점] 반품신청 기능 추가 #128

--- src/views/franchise/ReturnFormView.vue
+++ src/views/franchise/ReturnFormView.vue
@@ -22,10 +22,19 @@
         </tr>
       </thead>
       <tbody>
-        <template v-if="availableOrderItems.length > 0 && checkedOrderItems">
-          <tr v-for="orderItem in availableOrderItems" :key="orderItem.itemCode">
-            <td>
-              <Checkbox v-model="checkedOrderItems" name="availableOrderItems" :value="orderItem.itemCode" />
+        <template v-if="availableOrderItems.length > 0">
+          <tr
+            v-for="orderItem in availableOrderItems"
+            :key="orderItem.itemCode"
+            :class="{ highlight: checkedOrderItems.includes(orderItem.itemCode) }"
+          >
+            <td class="align-center">
+              <Checkbox
+                v-model="checkedOrderItems"
+                name="availableOrderItems"
+                :value="orderItem.itemCode"
+                size="small"
+              />
             </td>
             <td class="align-center">{{ orderItem.itemUniqueCode }}</td>
             <td class="align-center">{{ orderItem.itemName }}</td>
@@ -40,31 +49,42 @@
       </tbody>
     </AppTableStyled>
 
-    <AppRadioList
-      v-model="reason"
-      name="return-reason"
-      label="반품사유"
-      :options="reasonOptions"
-      label-position="left"
-    />
+    <div>
+      <AppRadioList v-model="reason" name="return-reason" label="반품사유" :options="reasonOptions" class="mb-3" />
+      <Textarea v-model="explanation" rows="10" cols="100" placeholder="반품 사유를 입력해주세요." size="small" />
+    </div>
 
-    <Textarea v-model="explanation" rows="10" cols="100" placeholder="반품 사유를 입력해주세요." size="small" />
+    <div>
+      <AppLabel label="반품 품목 사진을 올려주세요." use-margin-bottom />
+      <AppChooseFileList :upload-images="uploadFiles" @change-files="onChangeFiles" @remove-image="onRemove" />
+    </div>
 
-    {{ checkedOrderItems }}
+    <div class="action-buttons">
+      <Button label="취소" variant="outlined" severity="secondary" size="small" @click="$router.back()" />
+      <Button label="반품신청" size="small" @click="clickRequestReturn" />
+    </div>
   </div>
 </template>
 
 <script setup>
+import { useToast } from 'primevue';
 import { computed, ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
 
+import AppLabel from '@/components/common/AppLabel.vue';
 import AppTableStyled from '@/components/common/AppTableStyled.vue';
 import AppAutoComplete from '@/components/common/form/AppAutoComplete.vue';
-import AppCheck from '@/components/common/form/AppCheck.vue';
+import AppChooseFileList from '@/components/common/form/AppChooseFileList.vue';
 import AppRadioList from '@/components/common/form/AppRadioList.vue';
+import { useFiles } from '@/hooks/useFiles';
 import FCReturnApi from '@/utils/api/FCReturnApi';
 import { RETURN_REASON_LIST } from '@/utils/constant';
 import { formatKoReturnReason } from '@/utils/format';
 import { makeAutocompleteSuggestion, makeRadioOption } from '@/utils/helper';
+
+const { uploadFiles, onRemove, onChangeFiles } = useFiles();
+const toast = useToast();
+const router = useRouter();
 
 const selectedOrder = ref(null);
 const availableOrderItems = ref([]);
@@ -79,7 +99,7 @@ const reasonOptions = computed(() => {
 const explanation = ref('');
 
 const allCheck = ref(false);
-const checkedOrderItems = ref(null);
+const checkedOrderItems = ref([]); // 체크된 itemCode 목록이 들어감
 
 const fcReturnApi = new FCReturnApi();
 
@@ -90,6 +110,40 @@ const onCompleteInputOrderCode = event => {
   });
 };
 
+const checkForm = () => {
+  try {
+    if (!selectedOrder.value || !selectedOrder.value.code) throw new Error('반품할 주문을 입력해주세요');
+    if (checkedOrderItems.value.length === 0) throw new Error('반품신청할 품목을 선택해주세요');
+    if (!reason.value) throw new Error('반품사유를 선택해주세요');
+    if (!explanation.value) throw new Error('반품사유를 상세하게 작성해주세요.');
+    if (uploadFiles.value.length === 0) throw new Error('품목 상태를 확인할 이미지를 첨부해주세요.');
+
+    return true;
+  } catch (e) {
+    toast.add({ severity: 'error', summary: '입력 확인', detail: e.message, life: 3000 });
+    return false;
+  }
+};
+
+const clickRequestReturn = () => {
+  const isPass = checkForm();
+  if (!isPass) return;
+
+  fcReturnApi
+    .createReturn({
+      orderCode: selectedOrder.value.code,
+      returningItemCodeList: checkedOrderItems.value,
+      reason: reason.value,
+      explanation: explanation.value,
+      sumPrice: checkedOrderItems.value.reduce((acc, current) => acc + current.partSumPrice, 0),
+      imageFiles: uploadFiles.value.map(e => e.file),
+    })
+    .then(() => {
+      toast.add({ severity: 'success', summary: '처리 성공', detail: '반품신청이 등록되었습니다.', life: 3000 });
+      router.push({ name: 'fc:home:return:list' });
+    });
+};
+
 watch(selectedOrder, newSelectedOrder => {
   if (newSelectedOrder && newSelectedOrder.code) {
     const newSelectedOrderCode = newSelectedOrder.code;
@@ -97,29 +151,18 @@ watch(selectedOrder, newSelectedOrder => {
     // 반품 가능한 품목 조회
     fcReturnApi.getAvailableOrderItemsList(newSelectedOrderCode).then(data => {
       availableOrderItems.value = data;
-      // 초기 체크상태 설정
-      // checkedOrderItems.value = data.reduce((acc, current) => {
-      //   acc[current.itemCode] = true;
-      //   return acc;
-      // }, {});
     });
   }
 });
 
 watch(allCheck, newAllCheck => {
-  // if (newAllCheck) {
-  //   // 전체 체크상태로 변경
-  //   checkedOrderItems.value = availableOrderItems.value.reduce((acc, current) => {
-  //     acc[current.itemCode] = true;
-  //     return acc;
-  //   }, {});
-  // } else {
-  //   // 전체 체크 해제
-  //   checkedOrderItems.value = availableOrderItems.value.reduce((acc, current) => {
-  //     acc[current.itemCode] = false;
-  //     return acc;
-  //   }, {});
-  // }
+  if (newAllCheck) {
+    // 전체 체크상태로 변경
+    checkedOrderItems.value = availableOrderItems.value.map(e => e.itemCode);
+  } else {
+    // 전체 체크 해제
+    checkedOrderItems.value = [];
+  }
 });
 </script>
 
@@ -127,6 +170,11 @@ watch(allCheck, newAllCheck => {
 .return-form-container {
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 24px;
+
+  .action-buttons {
+    display: flex;
+    justify-content: space-between;
+  }
 }
 </style>
